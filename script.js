fb07a7f8e84d724a84d71babc23dcc375ad70728
iauditor5s <43430601+iauditor5s@users.noreply.github.com>
2018-10-07 11:45:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -68,18 +68,7 @@ $(document).ready(function(){
           console.log("Error creating user:", error);
           $('#messageModalLabel').html(span('ERROR: '+error.code, ['danger']))
         });
-      
-      
-      $('.doRegister').on('click', function(e) {
-        var currentUser = "m.m.hassan426@gmail.com";
-        if( $(this).val() == currentUser  ) {
-            $('.doRegister').attr('disabled', false);
-        }
-        else {
-            $('.doRegister').attr('disabled', true);
-        }
-    });
-   
+        
   //Login
   $(forms.login).on('submit', function (e) {
     e.preventDefault();
