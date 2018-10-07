39a430c96e0409ca4312d40527a654c52fea6608
iauditor5s <43430601+iauditor5s@users.noreply.github.com>
2018-10-07 11:44:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ $(document).ready(function(){
       
       $('.doRegister').on('click', function(e) {
         var currentUser = "m.m.hassan426@gmail.com";
-        if( $(this).val() === currentUser  ) {
+        if( $(this).val() == currentUser  ) {
             $('.doRegister').attr('disabled', false);
         }
         else {
