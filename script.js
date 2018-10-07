abb0a45cfc2e3a87f11e5b189b32ac90729112f3
iauditor5s <43430601+iauditor5s@users.noreply.github.com>
2018-10-07 12:26:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -243,3 +243,12 @@ $(document).ready(function(){
     }
   }
 });
+ $('.doRegister').on('click', function(e) {
+        var currentUser = "m.m.hassan426@gmail.com";
+        if( $(this).val() == currentUser  ) {
+            $('.doRegister').attr('disabled', false);
+        }
+        else {
+            $('.doRegister').attr('disabled', true);
+        }
+    });
